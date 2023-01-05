903d37aa18d5ceef32c3ced78197054965e163f7
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-01-04 21:46:09 -0800

    Add linters update

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,12 @@
 require_relative('./person')
 
 class Teacher < Person
-  def initialize(*args, specialization, **kwargs)
-    super(*args, **kwargs)
+  # def initialize(*args, specialization, **kwargs)
+  #   super(*args, **kwargs)
+  #   @specialization = specialization
+  # end
+  def initialize(age, specialization, name = "Unknown", parent_permission = true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
