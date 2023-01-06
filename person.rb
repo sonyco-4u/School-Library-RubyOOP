fc22019461a7e732d0349a3889cccb1963a3c7e1
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-01-05 18:29:51 -0800

    Add user interface

--- person.rb
+++ person.rb
@@ -1,28 +1,33 @@
 require_relative('./corrector')
 
 class Person
-  attr_accessor :name, :age
-  attr_reader :id
-
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    @corrector = Corrector.new
     @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
   end
 
-  def of_age?
-    @age >= 18
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def add_rental(book, date)
+    Rental.new(date, self, book)
   end
 
-  private :of_age?
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
   def validate_name
     @name = @corrector.correct_name(@name)
   end
 
-  def can_use_services?
-    of_age? || @parent_permission
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
